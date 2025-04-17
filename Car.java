19066e02e6c9b7b063f26e422babae0ce119c6f8
byczeczku <s33867@pjwstk.edu.pl>
2025-04-17 10:40:25 +0200

    wykonalem zad1

---
+++ Car.java
@@ -0,0 +1,19 @@
+public class Car extends Vehicle
+{
+    private int numberOfSeats;
+
+    public int getNumberOfSeats()
+    {
+        return numberOfSeats;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle
+{
+    public void start()
+    {
+        System.out.println("Samochod zostal uruchomiony :)");
+    }
+
+    public void stop()
+    {
+        System.out.println("Samochod zostal oduruchomiony/ wylaczony/ zatrzymany");
+    }
+}
